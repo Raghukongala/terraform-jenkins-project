379d9b5f7f9084770c3eb7d805358ce4ad62ba4b
Raghu <raghukongala406@gmail.com>
2026-04-02 10:07:22 +0530

    fix: consistent ../../../modules paths and clean Jenkinsfile

--- terraform-jenkins-project/envs/dev/main.tf
+++ terraform-jenkins-project/envs/dev/main.tf
@@ -29,20 +29,18 @@ locals {
 }
 
 module "vpc" {
-  source = "../../modules/vpc"
-
+  source             = "../../../modules/vpc"
   name               = local.name
   vpc_cidr           = var.vpc_cidr
   azs                = var.azs
   public_subnets     = var.public_subnets
   private_subnets    = var.private_subnets
-  enable_nat_gateway = false   # save cost in dev
+  enable_nat_gateway = false
   tags               = local.common_tags
 }
 
 module "s3_assets" {
-  source = "../../modules/s3"
-
+  source        = "../../../modules/s3"
   bucket_name   = "${local.name}-assets-${data.aws_caller_identity.current.account_id}"
   versioning    = false
   force_destroy = true
@@ -50,35 +48,33 @@ module "s3_assets" {
 }
 
 module "rds" {
-  source = "../../modules/rds"
-
-  name               = local.name
-  vpc_id             = module.vpc.vpc_id
-  subnet_ids         = module.vpc.private_subnet_ids
-  app_sg_ids         = [module.ec2.ec2_sg_id]
-  db_name            = var.db_name
-  db_username        = var.db_username
-  db_password        = var.db_password
-  instance_class     = "db.t3.micro"
-  multi_az           = false
+  source              = "../../../modules/rds"
+  name                = local.name
+  vpc_id              = module.vpc.vpc_id
+  subnet_ids          = module.vpc.private_subnet_ids
+  app_sg_ids          = [module.ec2.ec2_sg_id]
+  db_name             = var.db_name
+  db_username         = var.db_username
+  db_password         = var.db_password
+  instance_class      = "db.t3.micro"
+  multi_az            = false
   deletion_protection = false
   skip_final_snapshot = true
-  tags               = local.common_tags
+  tags                = local.common_tags
 }
 
 module "ec2" {
-  source = "../../modules/ec2"
-
-  name             = local.name
-  vpc_id           = module.vpc.vpc_id
-  subnet_ids       = module.vpc.private_subnet_ids
-  alb_sg_ids       = []
+  source            = "../../../modules/ec2"
+  name              = local.name
+  vpc_id            = module.vpc.vpc_id
+  subnet_ids        = module.vpc.private_subnet_ids
+  alb_sg_ids        = []
   target_group_arns = []
-  instance_type    = var.instance_type
-  min_size         = 1
-  max_size         = 2
-  desired_capacity = 1
-  tags             = local.common_tags
+  instance_type     = var.instance_type
+  min_size          = 1
+  max_size          = 2
+  desired_capacity  = 1
+  tags              = local.common_tags
 }
 
 data "aws_caller_identity" "current" {}
