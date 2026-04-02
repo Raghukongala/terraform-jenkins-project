66d177d7f24d848b78ef72cbe30b5d6a0e9d9315
Raghu <raghukongala406@gmail.com>
2026-04-02 09:50:01 +0530

    fix: rewrite variables.tf with valid HCL syntax

--- terraform-jenkins-project/envs/dev/variables.tf
+++ terraform-jenkins-project/envs/dev/variables.tf
@@ -1,10 +1,34 @@
-variable "aws_region"    { type = string; default = "ap-south-1" }
-variable "project_name"  { type = string; default = "myapp" }
-variable "vpc_cidr"      { type = string }
-variable "azs"           { type = list(string) }
-variable "public_subnets"  { type = list(string) }
-variable "private_subnets" { type = list(string) }
-variable "instance_type" { type = string; default = "t3.micro" }
-variable "db_name"       { type = string }
-variable "db_username"   { type = string }
-variable "db_password"   { type = string; sensitive = true }
+variable "aws_region" {
+  type    = string
+  default = "ap-south-1"
+}
+variable "project_name" {
+  type    = string
+  default = "myapp"
+}
+variable "vpc_cidr" {
+  type = string
+}
+variable "azs" {
+  type = list(string)
+}
+variable "public_subnets" {
+  type = list(string)
+}
+variable "private_subnets" {
+  type = list(string)
+}
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+variable "db_name" {
+  type = string
+}
+variable "db_username" {
+  type = string
+}
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
